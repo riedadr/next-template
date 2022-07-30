25e993ee873027de2699cda00ed6928426b34062
cit@GREGOR <riedadr@gmail.com>
2022-07-30 21:31:31 +0200

    tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,6 +17,8 @@ const Home: NextPage = () => {
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
+        <p>installed Extensions: <span className='text-draculaGreen'>Tailwind</span></p>
+
         <p className={styles.description}>
           Get started by editing{' '}
           <code className={styles.code}>pages/index.tsx</code>
